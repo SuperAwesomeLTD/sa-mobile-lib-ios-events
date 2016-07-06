1b0c383bb27c3262c7289b5fc004b092858cd493
Gabriel Coman <dev.gabriel.coman@gmail.com>
2016-07-06 14:46:26 +0100

    1.1.4

--- Example/SAEvents/SAViewController.m
+++ Example/SAEvents/SAViewController.m
@@ -25,7 +25,7 @@
         NSLog(@"%@ responds", @"abc");
     }
     
-    [SAEvents sendCustomEvent:@"https://ads.superawesome.tv/v2" withPlacement:7186 withLineItem:30285 andCreative:30169 andEvent:@"custom.PopJame"];
+    [SAEvents sendCustomEvent:@"https://ads.superawesome.tv/v2" withPlacement:7186 withLineItem:30285 andCreative:30169 andEvent:@"custom.PopJam"];
     [SAEvents sendEventToURL:@"https://ads.superawesome.tv/v2/video/impression?placement=28000&creative=-1&line_item=-1&sdkVersion=unknown&rnd=8720845"];
     [SAEvents sendEventToURL:@"https://ads.superawesome.tv/v2/video/tracking?event=start&placement=28000&creative=-1&line_item=-1&sdkVersion=unknown&rnd=9833859"];
     
