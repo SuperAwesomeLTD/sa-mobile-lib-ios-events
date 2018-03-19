16bee681df8a0fa9a1970047149ed5ad524da26f
Gabriel Coman <dev.gabriel.coman@gmail.com>
2018-03-19 12:15:09 +0000

    Add framework

---
+++ Pod/Libraries/MoatFramework.framework/Headers/MoatFramework.h
@@ -0,0 +1,19 @@
+//
+//  MoatFramework.h
+//  MoatFramework
+//
+//  Created by Gabriel Coman on 19/03/2018.
+//  Copyright © 2018 Gabriel Coman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MoatFramework.
+FOUNDATION_EXPORT double MoatFrameworkVersionNumber;
+
+//! Project version string for MoatFramework.
+FOUNDATION_EXPORT const unsigned char MoatFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MoatFramework/PublicHeader.h>
+
+
